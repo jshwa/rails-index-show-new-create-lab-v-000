9ef3c3f681c2022c45ccdca094752c1823bd0a5d
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-12 21:14:35 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :coupon, only: [:index, :new, :create, :show]
+  resources :coupon, only: [:new, :create, :show]
+  get :index, to: 'coupon#index', as: :coupons
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
